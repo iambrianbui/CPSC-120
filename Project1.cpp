5b8304e05e4695fafb940cb8ea9b0d919f427ce5
David Lee <SeaBear622@users.noreply.github.com>
2015-09-08 09:35:11 -0700

    Added comments

--- Project1.cpp
+++ Project1.cpp
@@ -1,8 +1,16 @@
+/* 
+	Project 1, A recipe qty and cost program
+	Created By: Brian Bui and David Lee
+	Created On: 9/5/2015
+	Last Updated: 9/8/2015
+*/
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	// Initialize Variables
 	double sandwichQty;
 	double breadQty;
 	double peanutButterQty;
@@ -11,6 +19,7 @@ int main()
 	double peanutButterCost;
 	double jellyCost;
 
+	// Constants for cost calulations
 	const double loafOfBreadCost = 5;
 	const int numberOfSlicesInLoaf = 18;
 	const double costOfPeanutButter = 2.50;
@@ -20,14 +29,18 @@ int main()
 	const int numberOfOuncesInJellyJar = 8;
 	const int numberOfGramsOfJellyInOneSandwich = 11;
 
+	// Ask user how many sandwichs
 	cout << "How many sandwiches do you wish to make?" << endl;
-
+	// Set variable to user input
 	cin >> sandwichQty;
 
+	// Get bread, peanut butter and jelly costs
+	// Takes cost by ounces converts to grams
 	breadCost = sandwichQty * 2 * loafOfBreadCost / numberOfSlicesInLoaf;
 	peanutButterCost = costOfPeanutButter / (numberOfOuncesInPeanutButterJar / 0.035274) * numberOfGramsOfPeanutButterInOneSandwich * sandwichQty;
 	jellyCost = costOfJelly / (numberOfOuncesInJellyJar / 0.035274) * numberOfGramsOfJellyInOneSandwich * sandwichQty;
 
+	// Display results
 	cout << endl;
 	cout << "In order to make " << sandwichQty << " PP&Js" << endl;
 	cout << "You will need " << sandwichQty * 2 << " slices of bread which will cost you $" << breadCost << endl;
@@ -37,6 +50,7 @@ int main()
 	cout << endl;
 	cout << endl;
 
+	// Pause system to read results
 	system("pause");
 	return 0;
 }
